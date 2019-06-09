3def8929dd465097a0db2a9e71e266f5513f80b7
Adrián Rougouski <32654123+adrian-arg@users.noreply.github.com>
2019-06-09 15:29:39 -0300

    Punto Candidato

--- src/cmc/PuntoCandidato.java
+++ src/cmc/PuntoCandidato.java
@@ -2,32 +2,46 @@ package cmc;
 
 import graficos.Punto;
 
-public class PuntoCandidato {
+public class PuntoCandidato implements Comparable{
 	
-	private int costoDistancia;
-	private int costoPeso;
-	private int costoTotal;
+	private double costoDistancia;
+	private double costoPeso;
+	private double costoTotal;
 	private int x, y;
-	private Punto punto;
-	private Punto predecesor;
 	private boolean abierto; //Abierto == true , Cerrado == false
+	private Punto punto;
+	private PuntoCandidato predecesor;
 	
-	public PuntoCandidato(int costoDistancia, int costoPeso, int x, int y, 
-			Punto predecesor) {
-		this.costoDistancia = costoDistancia;
+	public PuntoCandidato(int costoPeso, int x, int y, 
+			PuntoCandidato predecesor, Punto destino) {
+		this.costoDistancia = calcularDistanciaADestino(destino);
 		this.costoPeso = costoPeso;
-		this.costoTotal = costoTotal;
+		this.costoTotal = calcularCostoTotal();
+		/* PuntoCandidato */
 		this.x = x;
 		this.y = y;
-		this.punto = punto;
+		this.punto = new Punto(x,y);
+		/* PuntoCandidato */
 		this.predecesor = predecesor;
-		this.abierto = abierto;
+		this.abierto = true;
 	}
 	
+	private double calcularDistanciaADestino(Punto destino) {
+		return Math.abs(destino.getX() - this.x) + Math.abs(destino.getY() - this.y); 
+	}
+
+	public double getCostoTotal() {
+		return costoTotal;
+	}
 	
+	public double calcularCostoTotal() {
+		return this.costoDistancia + this.costoPeso;
+	}
 	
-	
-	
-	
-	
+	@Override
+	public int compareTo(Object otroPunto) {
+		double diferencia = this.costoTotal - ((PuntoCandidato) otroPunto).getCostoTotal();
+		return (int)diferencia;
+	}
+
 }
